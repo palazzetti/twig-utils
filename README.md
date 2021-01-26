d83d62378bb40489b93f9932cb694b3c2e9b56d4
Palazzetti Lelio Spa <49283657+palazzetti@users.noreply.github.com>
2021-01-26 12:19:19 +0100

    Added ucfirst util

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Twig Palazzetti Utilities Extension
 This package is a Twig extension that provides the following:
 
  * [`unique`][1] filter: array unique php method.
+ * [`ucfirst`][2] transformation: ucfirst php method.
 
 [1]: https://www.php.net/array_unique
+[2]: https://www.php.net/ucfirst
---
+++ src/TextUtilsExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Twig Utils Package.
+ *
+ * (c) Daniele Sarnari <daniele.sarnari@palazzetti.it>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Twig\Utils\Templating;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class TextUtilsExtension extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('ucfirst', 'Twig\\Utils\\Templating\\twig_text_ucfirst', ['is_safe' => ['all']]),
+        ];
+    }
+}
+
+function twig_text_ucfirst(string $text): string
+{
+    return ucfirst($text);
+}
